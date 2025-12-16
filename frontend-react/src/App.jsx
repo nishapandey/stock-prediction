b4d11f3d584ac1c309fa0b095eb16f619fb13b65
npandey <npandey@redhat.com>
2025-12-15 23:38:08 -0500

    initial commit

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Stock Prediction Portal</h1>
+    </>
+  )
+}
+
+export default App

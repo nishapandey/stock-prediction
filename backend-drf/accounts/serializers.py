604c4ec3514be348b3ce637dd0ed51ba861631cf
npandey <npandey@redhat.com>
2025-12-27 11:47:41 -0500

    initial commit

---
+++ backend-drf/accounts/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        read_only_fields = ['id']
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password_confirm', 'first_name', 'last_name']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError({'password_confirm': 'Passwords do not match.'})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(**validated_data)
+        return user
+
---
+++ notes.md
@@ -0,0 +1,32 @@
+
+Notes:
+activate virtual env 
+source venv/bin/activate
+
+react commands
+
+npm create vite@latest
+
+npm install
+
+npm run dev
+
+
+django commands:
+https://djecrety.ir/
+https://pypi.org/project/python-decouple/
+pip install djangorestframework
+
+django-admin startproject stock_prediction_main .
+
+python manage.py runserver
+
+python manage.py migrate
+
+ python manage.py  startapp accounts
+
+
+
+django apps flow
+
+model > serialization > views > urls 
